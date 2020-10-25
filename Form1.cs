990a449407d8d76308077aa48b62c75aadf2ca7f
Yasi1995 <yasasi888@gmail.com>
2020-10-25 08:50:59 +0530

    first commit

---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab01
+{
+
+    public partial class Form1 : Form
+    {
+        Preprocessing p = new Preprocessing();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void load_Click(object sender, EventArgs e)
+        {
+            p.LoadOriginalImage();
+            pictureBox1.ImageLocation = "1.jpg";
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            p.GrayScalelImage();
+            pictureBox2.ImageLocation = "2.jpg";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            p.ExtractImage();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            p.ExtractImage();
+
+        }
+    }
+}
---
+++ Preprocessing.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCvSharp;
+
+namespace lab01
+{
+    class Preprocessing
+    {
+        IplImage scr,gray;
+        public void LoadOriginalImage()
+        {
+            scr = Cv.LoadImage("apple1.jpg", LoadMode.Color);
+            Cv.SaveImage("1.jpg", scr);
+
+        }
+        public void GrayScalelImage()
+        {
+            gray = Cv.CreateImage(scr.Size, BitDepth.U8, 1);
+            Cv.CvtColor(scr, gray, ColorConversion.RgbaToGray);
+            Cv.SaveImage("2.jpg", gray);
+        }
+        public void ExtractImage()
+        {
+            System.Windows.Forms.MessageBox.Show("Height :" + scr.Height + ", Width:" + scr.Width + " ,No of Channels:" + scr.NChannels);
+        }
+
+
+    }
+}
